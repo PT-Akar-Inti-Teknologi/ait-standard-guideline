e80ee44c9b471e8787cb38b5c7d8907d765a59eb
fadhylfa <fadhyl.alghifari@akarinti.tech>
2022-10-13 10:15:18 +0700

    feat: add dependency h2 database

---
+++ src/test/java/org/ait/project/guideline/example/modules/post/controller/JsonPlaceHolderPostControllerTest.java
@@ -0,0 +1,30 @@
+package org.ait.project.guideline.example.modules.post.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class JsonPlaceHolderPostControllerTest {
+
+    @Test
+    void getAllJsonPlaceHolderPost() {
+    }
+
+    @Test
+    void getAllJsonPlaceHolderPostPage() {
+    }
+
+    @Test
+    void getJsonPlaceHolderPostById() {
+    }
+
+    @Test
+    void addPost() {
+    }
+}

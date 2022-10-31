063afa2630de5f6858a7bc0aac9e1edb3994ed51
bintang-dev <bintang@akarinti.tech>
2022-10-31 15:30:56 +0700

    refactor: little revisions

--- src/test/java/org/ait/project/guideline/example/ApplicationTests.java
+++ src/test/java/org/ait/project/guideline/example/ApplicationTests.java
@@ -2,6 +2,9 @@ package org.ait.project.guideline.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 class ApplicationTests {
